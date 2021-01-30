1ba41d03a217cca374514b5bce025fdaad326123
C_PC <2371270263@qq.com>
2021-01-30 15:56:35 +0800

    修改DS18B20部分代码格式

--- drive/AT24C08.h
+++ drive/AT24C08.h
@@ -5,24 +5,13 @@
 #define u16 unsigned short
 #define u32 unsigned int
 
-
-
 #define AT24C08_ADDRESS                 (0xA0)                                   //AT24C02 device address
-#define AT24C08_BLOCK0_ADDRESS          (AT24C08_ADDRESS|0x00)
-#define AT24C08_BLOCK1_ADDRESS          (AT24C08_ADDRESS|0x02)
-#define AT24C08_BLOCK2_ADDRESS          (AT24C08_ADDRESS|0x04)
-#define AT24C08_BLOCK3_ADDRESS          (AT24C08_ADDRESS|0x06)
-#define AT24C08_READ_DATA                0x01
-#define AT24C08_WRITE_DATA               0x00
-
 
 void AT24C08_init(void);
 u8 AT24C08Read_Byte(u8 dev_addr,u8 Data_addr);
 void AT24C08Write_Byte(u8 dev_addr,u8 Data_addr,u8 data);
 void AT24C08Read_NBytes(u8 dev_addr,u8 addr,u8 length,u8 *data);
 void AT24C08Write_NBytes(u8 dev_addr,u8 addr,u8 length,u8 *data);
-
-
 
 
 #endif
--- drive/ds18b20.c
+++ drive/ds18b20.c
@@ -6,25 +6,22 @@
 
                              //PB8 --------DATE
 
-void gpio_out(u8 state)
+static void GPIO_Out(u8 state)
 {
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB,ENABLE);
 
 	GPIO_InitTypeDef GPIOB_Initstruc;
-	
-	
+
 	GPIOB_Initstruc.GPIO_Pin=GPIO_Pin_8;                       
 	GPIOB_Initstruc.GPIO_Mode=GPIO_Mode_OUT;
 	GPIOB_Initstruc.GPIO_Speed=GPIO_Speed_50MHz;         
 	GPIOB_Initstruc.GPIO_PuPd=GPIO_PuPd_UP;
-	
 	GPIO_Init(GPIOB,&GPIOB_Initstruc);
 
 	if(state==1)
 	{	
 	GPIO_SetBits(GPIOB,GPIO_Pin_8);
-
 	}
 	else 
 	{	
@@ -36,7 +33,7 @@ void gpio_out(u8 state)
 
 
 
-void gpio_in()
+static void GPIO_In()
 {
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB,ENABLE);
@@ -61,19 +58,19 @@ void ds18b20_write_byte(u32 data)
 u8 i;
 	for(i=0;i<8;i++)
 	{
-	gpio_out(0); 
+	GPIO_Out(0); 
 	delay_us(15);		
 		if((0x01&data)==0)
 		{		
-		gpio_out(0); 
+		GPIO_Out(0); 
 		}
 		else
 		{		
-		gpio_out(1); 
+		GPIO_Out(1); 
 		}
 	delay_us(55);
 	
-	gpio_out(1); 
+	GPIO_Out(1); 
 	data=data>>1;
 	delay_us(3);
 	}
@@ -87,69 +84,46 @@ char i;
   u32 	temp=0;		
 	for(i=0;i<8;i++)
 	{
-	gpio_out(0);
+	GPIO_Out(0);
 	delay_us(8);
-	gpio_out(1);
-	gpio_in();		
+	GPIO_Out(1);
+	GPIO_In();		
 	if(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_8)==1)	
 	temp=0x80;
-  else               							
+  	else               							
 	temp=0x00;	
 	delay_us(40);
 	data=data|(temp>>(7-i));
-	gpio_out(1);
+	GPIO_Out(1);
 	delay_us(4);
 	}
-
-
-	
 	return data;
 
 }
 	
 int ds18B20_init(char CMD)
 {
-	
-u8 flag=0;
+	u8 flag=0;
 
-	gpio_out(1); 
+	GPIO_Out(1); 
 	delay_us(10);
-  gpio_out(0);            //拉低总线		
+  	GPIO_Out(0);        //拉低总线		
 	delay_us(500);
-	gpio_out(1);      	//释放总线
-	gpio_in();
-	delay_us(120);																	//延时检测
-	
+	GPIO_Out(1);      	//释放总线
+	GPIO_In();
+	delay_us(120);		//延时检测
 	
 	if(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_8)==0)        //18b20应答检测
 	{	
-
-	flag=1;
+		flag=1;
 	}
 	else
-	flag=0;
-
-	gpio_out(1);
-	
-	if(CMD==1)
 	{
-			if(flag==1)
-			{
-			//display_str_and_speed("OK",10);
-
-			}
-			else
-			{
-			//display_str_and_speed("ERROR",10);
-
-			}
-		}
-		if(flag==1)
-				return 0;	
-			else
-				return 1;		
-
-
+		flag=0;
+	}
+	GPIO_Out(1);
+	
+	return flag;
 	
 }	
 	
@@ -216,14 +190,6 @@ void tempure_value_OLEDdisplay(char x,char y)
 	
 	}
 
-
-
-
-
-
-
-
-
 	//	return A1*100+A2*10+A3;      需要返回温度时用
 
 }	
@@ -250,8 +216,7 @@ void Get_Temperature(u8 *Temperature)
 		LSB=ds18b20_read_byte();
 		MSB=ds18b20_read_byte();
 	
-	
-
+	s
 	wendu=(LSB|((MSB&0x7)<<8))*625;
 	*Temperature=(u8)(wendu/10000);
 		
