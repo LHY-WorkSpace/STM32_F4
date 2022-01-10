88d0f931b435b4612d5f9d98009cdfc596b7d512
LHY-WorkSpace <2371270263@qq.com>
2022-01-10 22:55:45 +0800

    测试滚动

--- Module/u8g2_src/u8g2_UserGUI.c
+++ Module/u8g2_src/u8g2_UserGUI.c
@@ -61,7 +61,7 @@ void Safe_icon()
                k=9; 
             }
         } while (u8g2_NextPage(&u8g2));
-        vTaskDelayUntil(&Time,300/portTICK_PERIOD_MS);
+        vTaskDelayUntil(&Time,500/portTICK_PERIOD_MS);
     }
 
 }
@@ -84,10 +84,55 @@ void Dir_icon()
                 k=0;
             }
         } while (u8g2_NextPage(&u8g2));
+
         vTaskDelayUntil(&Time,100/portTICK_PERIOD_MS);
     }
 
 }
+
+void Streamline_icon()
+{
+    u8 k=0,p=0;
+    TickType_t Time;	
+    Time=xTaskGetTickCount();
+    while (1)
+    {
+        //u8g2_FirstPage(&u8g2);
+        do
+        {	
+            u8g2_SetFont(&u8g2, u8g2_font_streamline_all_t);
+            u8g2_SetFontDirection(&u8g2, 0);
+            u8g2_DrawGlyph(&u8g2,0,60,0x30+(k+1));
+            u8g2_DrawGlyph(&u8g2,22,60,0x30+(k+2));
+            u8g2_DrawGlyph(&u8g2,44,60,0x30+(k+3));
+            u8g2_DrawGlyph(&u8g2,66,60,0x30+(k+4));
+            u8g2_DrawGlyph(&u8g2,88,60,0x30+(k+5));
+            k++;
+        } while (u8g2_NextPage(&u8g2));
+        vTaskDelayUntil(&Time,100/portTICK_PERIOD_MS);
+    }
+}
+
+void Shift_icon()
+{
+    u8 k=0,p=;
+    TickType_t Time;	
+    Time=xTaskGetTickCount();
+    while (1)
+    {
+        do
+        {	
+            u8g2_SetFont(&u8g2, u8g2_font_streamline_all_t);
+            u8g2_SetFontDirection(&u8g2, 0);
+            u8g2_DrawGlyph(&u8g2,k,40,0x29D);
+            k++;
+            if(k>=128)
+            k=0;
+        } while (u8g2_NextPage(&u8g2));
+        vTaskDelayUntil(&Time,50/portTICK_PERIOD_MS);
+    }
+}
+
 
 
 
@@ -125,6 +170,8 @@ void u8g2_TaskCreate()
     xTaskCreate( (TaskFunction_t)LED_Task,    "IconTask",200,NULL,10,            NULL);
     xTaskCreate( (TaskFunction_t)Safe_icon,    "IconTask",200,NULL,10,            NULL);
     xTaskCreate( (TaskFunction_t)Dir_icon,    "IconTask",200,NULL,10,            NULL);
+    xTaskCreate( (TaskFunction_t)Streamline_icon,    "IconTask",200,NULL,10,            NULL);
+    xTaskCreate( (TaskFunction_t)Shift_icon,    "IconTask",200,NULL,10,            NULL);
 }
 
 
