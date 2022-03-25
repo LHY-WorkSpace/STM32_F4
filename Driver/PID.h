ee3be18bbd0d071f9adb45dd178cbe5a5a6dd30a
lihaiyan <1607762095@qq.com>
2022-03-25 10:59:51 +0800

    添加计划路线

--- Driver/PID.h
+++ Driver/PID.h
@@ -1,7 +1,15 @@
 #ifndef PID_H
 #define PID_H
 
-
+struct _pid
+{
+	float SetSpeed;//定义设定值
+	float ActualSpeed;//定义实际值
+	float err;//定义偏差值
+	float err_next;//定义上一个偏差值
+	float err_last;//定义最上前的偏差值
+	float Kp, Ki, Kd;//定义比例、积分、微分系数
+}pid;
 
 
 void PID_init(void );
