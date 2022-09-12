bb4c01edb641cf654cb63dde76e597548d74eafe
LHY-WorkSpace <2371270263@qq.com>
2022-09-12 22:51:42 +0800

    2022-09-12 1.ADC添加定时触发功能

--- Driver/FFT.c
+++ Driver/FFT.c
@@ -4,40 +4,32 @@
 //屏幕长度
 #define  SCREEN_LENGTH   (128)
 //采样点数
-#define  POINT   		(1024)
+#define  POINT   		(512)
 
 u16 ADC_Data[POINT];
 FFT_Data_t FFT_Data[POINT];
 float32_t FFT_Display[SCREEN_LENGTH];
 u8 PointY[SCREEN_LENGTH];//记录上次的点位置
 u8 DMADataReady = RESET;
-
+u8 ADC_DataFlag = RESET;
 static u8g2_t u8g2_Data;
 
 
 //  PA1 - ADC1_CH1 -  DMA2_S0_CH0
-//目前采用ADC一直连续转换，DMA一直传送数据，没有使用定时器定时采样，可根据DMA中断，判断数据是否完成
+//	10us 触发一次
 void ADCTimer_Init()
 {
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStr;
-	NVIC_InitTypeDef  NVIC_Initstr;
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);
 	
 	TIM_TimeBaseInitStr.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseInitStr.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseInitStr.TIM_Period = 20-1;
-	TIM_TimeBaseInitStr.TIM_Prescaler = 42000-1;
+	TIM_TimeBaseInitStr.TIM_Period = 10-1;
+	TIM_TimeBaseInitStr.TIM_Prescaler = 84-1;
     TIM_TimeBaseInit(TIM2,&TIM_TimeBaseInitStr);
 
-	NVIC_Initstr.NVIC_IRQChannel=TIM2_IRQn;
-	NVIC_Initstr.NVIC_IRQChannelPreemptionPriority=2;
-	NVIC_Initstr.NVIC_IRQChannelSubPriority=0;
-	NVIC_Initstr.NVIC_IRQChannelCmd=ENABLE;
-	NVIC_Init(&NVIC_Initstr);
-
     TIM_ARRPreloadConfig(TIM2,ENABLE);
-    TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
-    TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE);
+	TIM_SelectOutputTrigger(TIM2,TIM_TRGOSource_Update);
     TIM_Cmd(TIM2,ENABLE);
 }
 
@@ -97,36 +89,37 @@ void FFT_ADCInit()
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);	
 	
-	//NVIC_InitTypeDef  NVIC_Initstr;
+	// NVIC_InitTypeDef  NVIC_Initstr;
 	GPIO_InitTypeDef GPIO_Initstruc;
 	ADC_InitTypeDef   ADC_InitTypeDefstruct;
 	ADC_CommonInitTypeDef   ADC_CommonInitTypeDefstruct;
 
 	GPIO_Initstruc.GPIO_Pin=GPIO_Pin_1;
 	GPIO_Initstruc.GPIO_Mode=GPIO_Mode_AN; 
-	GPIO_Initstruc.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Initstruc.GPIO_Speed=GPIO_Speed_100MHz;
 	GPIO_Init(GPIOA,&GPIO_Initstruc);
 
 	ADC_CommonInitTypeDefstruct.ADC_Mode = ADC_Mode_Independent;
-	ADC_CommonInitTypeDefstruct.ADC_Prescaler = ADC_Prescaler_Div6;                                   //PLCLK 8分频
+	ADC_CommonInitTypeDefstruct.ADC_Prescaler = ADC_Prescaler_Div2;                                   //PLCLK 8分频
 	ADC_CommonInitTypeDefstruct.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
 	ADC_CommonInitTypeDefstruct.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_10Cycles;
 	ADC_CommonInit(&ADC_CommonInitTypeDefstruct);
 
 	ADC_InitTypeDefstruct.ADC_Resolution = ADC_Resolution_12b;              
 	ADC_InitTypeDefstruct.ADC_ScanConvMode = DISABLE;
-	ADC_InitTypeDefstruct.ADC_ContinuousConvMode = ENABLE;
-	ADC_InitTypeDefstruct.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+	ADC_InitTypeDefstruct.ADC_ContinuousConvMode = DISABLE;
+	ADC_InitTypeDefstruct.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_Rising;
 	ADC_InitTypeDefstruct.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T2_TRGO;
 	ADC_InitTypeDefstruct.ADC_DataAlign = ADC_DataAlign_Right;
 	ADC_InitTypeDefstruct.ADC_NbrOfConversion = 1;
 	ADC_Init(ADC1,&ADC_InitTypeDefstruct);
 
+
 	ADC_DMARequestAfterLastTransferCmd(ADC1,ENABLE);
 	ADC_RegularChannelConfig(ADC1,ADC_Channel_1,1,ADC_SampleTime_28Cycles);
 	ADC_DMACmd(ADC1,ENABLE);
 	ADC_Cmd(ADC1,ENABLE);
-	ADC_SoftwareStartConv(ADC1);
+	// ADC_SoftwareStartConv(ADC1);
 
 }
 
@@ -140,7 +133,7 @@ void FFT_Init()
 
 	DMA_ConfigInit();
 	FFT_ADCInit();
-	// ADCTimer_Init();
+	ADCTimer_Init();
 
 	DMADataReady = RESET;
 }
@@ -160,7 +153,7 @@ void DisplayMode_1(u8g2_t *u8g2_Data)
 	for ( i = 0; i < SCREEN_LENGTH; i++)
 	{
 		//非线性放大，主要放大高频区域
-		High = (u8)(log10(i+2)*FFT_Display[i+1]/500);
+		High = (u8)(2*log10(i+3)*FFT_Display[i+1]/500);
 
 		if( PointY[i] < High)
 		{
@@ -201,13 +194,13 @@ void DisplayMode_2(u8g2_t *u8g2_Data)
 	for ( i = 0; i < SCREEN_LENGTH/2; i++)
 	{
 		//非线性放大，主要放大高频区域
-		if(i<15)
+		if(i<10)
 		{
-			High = (u8)(FFT_Display[i+1]/600);
+			High = (u8)(FFT_Display[i+1]/800);
 		}
 		else
 		{
-			High = (u8)(2*log10(i-10)*FFT_Display[i+1]/500);
+			High = (u8)(2*log10(i-7)*FFT_Display[i+1]/500);
 		}
 
 		if( PointY[i] < High)
@@ -276,8 +269,19 @@ void DisplayMode_4(u8g2_t *u8g2)
 
 	for ( i = 0; i < SCREEN_LENGTH/2; i++)
 	{
+		// //非线性放大，主要放大高频区域
+		// High = (u8)log10(i)*FFT_Display[i]/500;
+
 		//非线性放大，主要放大高频区域
-		High = (u8)log10(i)*FFT_Display[i]/500;
+		if(i<10)
+		{
+			High = (u8)(FFT_Display[i+1]/800);
+		}
+		else
+		{
+			High = (u8)(log(i-7)*FFT_Display[i+1]/500);
+		}
+
 
 		if( PointY[i] < High)
 		{
@@ -332,7 +336,7 @@ void FFT_Process()
 		}
 
 		//1024点FFT处理
-		arm_cfft_f32(&arm_cfft_sR_f32_len1024,(float32_t *)FFT_Data,0,1);
+		arm_cfft_f32(&arm_cfft_sR_f32_len512,(float32_t *)FFT_Data,0,1);
 
 		//取幅值
 		arm_cmplx_mag_f32((float32_t *)FFT_Data,FFT_Display,SCREEN_LENGTH);
@@ -388,6 +392,13 @@ void DMA2_Stream0_IRQHandler()
 	{
 		DMA_ClearITPendingBit(DMA2_Stream0,DMA_IT_TCIF0);
 		DMADataReady = SET;
+
+
+
+
+
+
+
 	}
 }
 
@@ -397,16 +408,29 @@ void TIM2_IRQHandler()
     {
 		TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
 
-		// FFT_Process();
-		// u8g2_DispalyFFT(&u8g2_Data,3);
+		if( ADC_DataFlag == RESET)
+		{
+			ADC_DataFlag = SET;
+			LED1_ON;
+		}
+		else
+		{	
+			ADC_DataFlag = RESET;
+			LED1_OFF;
+		}
 
     }
 }
 
-// void ADC_IRQHandler()
-// {
-// 	if( ADC_GetITStatus(ADC1,ADC_IT_EOC) == SET) 
-// 	{
-// 		ADC_ClearITPendingBit(ADC1,ADC_IT_EOC);
-// 	}
-// }
+void ADC_IRQHandler()
+{
+	if( ADC_GetITStatus(ADC1,ADC_IT_EOC) == SET) 
+	{
+		ADC_ClearITPendingBit(ADC1,ADC_IT_EOC);
+
+
+
+
+
+	}
+}
