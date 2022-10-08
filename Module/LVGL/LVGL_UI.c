ba454fbfa5a94c96fc609b8e5111485e46ebadd9
LHY-WorkSpace <2371270263@qq.com>
2022-10-08 23:05:34 +0800

    1.LVGL 添加蒙版滚轮测试

--- Module/LVGL/LVGL_UI.c
+++ Module/LVGL/LVGL_UI.c
@@ -237,6 +237,99 @@ void LVGL_Line()
 }
 
 
+static void mask_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);        //获取事件
+    lv_obj_t * obj = lv_event_get_target(e);            //获取当前事件
+
+    static int16_t mask_top_id = -1;                    //定义顶部遮罩
+    static int16_t mask_bottom_id = -1;                 //定义底部遮罩
+
+    if (code == LV_EVENT_COVER_CHECK) {                 //事件覆盖检查
+        lv_event_set_cover_res(e, LV_COVER_RES_MASKED); //设置封面检查结果
+
+    } else if (code == LV_EVENT_DRAW_MAIN_BEGIN) {      //执行主绘图
+        /* add mask */
+        const lv_font_t * font = lv_obj_get_style_text_font(obj, LV_PART_MAIN);     //获取对象字体
+        lv_coord_t line_space = lv_obj_get_style_text_line_space(obj, LV_PART_MAIN);//获取对象字体行间距
+        lv_coord_t font_h = lv_font_get_line_height(font);  //获取行高
+
+        lv_area_t roller_coords;                        //设置绘制区域变量
+        lv_obj_get_coords(obj, &roller_coords);         //复制一个对象的坐标到一个区域
+
+        lv_area_t rect_area;                            //设置绘制区域变量
+        rect_area.x1 = roller_coords.x1;                //获取到x1的值
+        rect_area.x2 = roller_coords.x2;                //获取到x2的值
+        rect_area.y1 = roller_coords.y1;                //获取到y1的值
+        rect_area.y2 = roller_coords.y1 + (lv_obj_get_height(obj) - font_h - line_space) / 2;           //获取到y2的值
+
+        lv_draw_mask_fade_param_t * fade_mask_top = lv_mem_buf_get(sizeof(lv_draw_mask_fade_param_t));  //绘制蒙版褪色参数 顶部
+        
+        lv_draw_mask_fade_init(fade_mask_top, &rect_area, LV_OPA_TRANSP, rect_area.y1, LV_OPA_COVER, rect_area.y2);     //初始化一个淡出遮罩。
+        mask_top_id = lv_draw_mask_add(fade_mask_top, NULL);        //添加一个顶部绘制蒙版
+
+        rect_area.y1 = rect_area.y2 + font_h + line_space - 1;      //获取y1坐标
+        rect_area.y2 = roller_coords.y2;                            //获取y2坐标
+
+        lv_draw_mask_fade_param_t * fade_mask_bottom =lv_mem_buf_get(sizeof(lv_draw_mask_fade_param_t));//绘制蒙版褪色参数 底部
+
+        lv_draw_mask_fade_init(fade_mask_bottom, &rect_area, LV_OPA_COVER, rect_area.y1, LV_OPA_TRANSP, rect_area.y2);  //初始化一个淡出遮罩。
+        mask_bottom_id = lv_draw_mask_add(fade_mask_bottom, NULL);  //添加一个顶部绘制蒙版
+
+    } else if (code == LV_EVENT_DRAW_POST_END) {                    //完成post绘制阶段(当所有子元素都绘制时)
+        lv_draw_mask_fade_param_t * fade_mask_top = lv_draw_mask_remove_id(mask_top_id);        //移除蒙版        
+        lv_draw_mask_fade_param_t * fade_mask_bottom = lv_draw_mask_remove_id(mask_bottom_id);  //移除蒙版
+        lv_draw_mask_free_param(fade_mask_top);                     //释放参数中的数据。        
+        lv_draw_mask_free_param(fade_mask_bottom);                  //释放参数中的数据。
+        lv_mem_buf_release(fade_mask_top);                          //手动释放内存    
+        lv_mem_buf_release(fade_mask_bottom);                       //手动释放内存
+        mask_top_id = -1;                                           
+        mask_bottom_id = -1;
+    }
+}
+
+/*************************************************
+ *  函数名称 :  roller_show_3
+ *  参    数 ： e 
+ *  函数功能 ： 滚动回调显示
+ *************************************************/
+void roller_show_3(void)
+{
+    static lv_style_t style;                            //创建样式变量
+    lv_style_init(&style);                              //初始化样式
+    lv_style_set_bg_color(&style, lv_color_black());    //设置背景颜色
+    lv_style_set_text_color(&style, lv_color_white());  //设置字体颜色
+    lv_style_set_border_width(&style, 0);               //设置边框宽度
+    lv_style_set_pad_all(&style, 0);                    //设置边距
+    lv_obj_add_style(lv_scr_act(), &style, 0);          //添加样式
+
+    lv_obj_t *roller1 = lv_roller_create(lv_scr_act()); //创建roller对象
+    lv_obj_set_pos(roller1,0,100);
+    lv_obj_add_style(roller1, &style, 0);               //添加样式
+    lv_obj_set_style_bg_opa(roller1, LV_OPA_TRANSP, LV_PART_SELECTED);  //设置对象的透明度
+
+   // lv_obj_set_style_text_font(roller1, &lv_font_montserrat_12, LV_PART_SELECTED);  //设置对象字体
+
+    lv_roller_set_options(roller1,                      //设置对象选项
+                        "January\n"
+                        "February\n"
+                        "March\n"
+                        "April\n"
+                        "May\n"
+                        "June\n"
+                        "July\n"
+                        "August\n"
+                        "September\n"
+                        "October\n"
+                        "November\n"
+                        "December",
+                        LV_ROLLER_MODE_NORMAL);
+
+    // lv_obj_center(roller1);                             //居中对象
+    lv_roller_set_visible_row_count(roller1, 3);        //设置对象间距
+    lv_obj_add_event_cb(roller1, mask_event_cb, LV_EVENT_ALL, NULL);   //设置回调
+}
+
 
 
 
@@ -299,7 +392,8 @@ void LVGL_Init()
 	lv_port_indev_init();
 #endif
 
-    LVGL_Build_GUI();
+   // LVGL_Build_GUI();
+    roller_show_3();
     // LVGL_Demo();
 }
 
