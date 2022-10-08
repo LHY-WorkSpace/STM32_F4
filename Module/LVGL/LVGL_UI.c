e72949c7fe1e583fe427e7e97afe49b5139e6bef
Decktop <2371270263@qq.com>
2022-10-08 16:06:10 +0800

    1.添加LVGL兼容代码文件

---
+++ Module/LVGL/LVGL_UI.c
@@ -0,0 +1,323 @@
+#include "LVGL_UI.h"
+
+#ifdef ON_PC
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include "lvgl/lvgl.h"
+    #include "lv_drivers/win32drv/win32drv.h"
+    #include <windows.h>
+#else
+    #include "IncludeFile.h"
+#endif
+
+
+lv_obj_t *Btn_OK;
+lv_obj_t *Btn_BACK;
+lv_obj_t *Btn_DOWN;
+lv_obj_t *Btn_UP;
+lv_obj_t *Bg;
+
+
+
+//***************************************************//
+//  功能描述: LVGL 按键处理
+//  
+//  参数: lv_event_t 
+//  
+//  返回值: lv_event_cb_t
+//  
+//  说明: 无
+//  
+//***************************************************//
+lv_event_cb_t OKKeyProcess(lv_event_t *Event)
+{
+    lv_event_code_t Code = lv_event_get_code(Event);
+
+    if( Code == LV_EVENT_CLICKED)
+    {
+        printf("OK !! \r\n");
+        lv_obj_set_style_bg_color(Bg,lv_color_white(),LV_PART_MAIN);
+    }
+}
+
+
+lv_event_cb_t DownKeyProcess(lv_event_t *Event)
+{
+    lv_event_code_t Code = lv_event_get_code(Event);
+
+    lv_obj_t *Target = lv_event_get_target(Event);
+
+    if( Code == LV_EVENT_CLICKED)
+    {
+        printf("Down !! \r\n");
+        lv_obj_set_style_bg_color(Bg,lv_palette_main(LV_PALETTE_GREEN),LV_PART_MAIN);
+    }
+}
+
+lv_event_cb_t UpKeyProcess(lv_event_t *Event)
+{
+    lv_event_code_t Code = lv_event_get_code(Event);
+
+    if( Code == LV_EVENT_CLICKED)
+    {
+        printf("Up !! \r\n");
+        lv_obj_set_style_bg_color(Bg,lv_palette_main(LV_PALETTE_BLUE),LV_PART_MAIN);
+        // lv_obj_add_flag(Roll,LV_OBJ_FLAG_SCROLL_ONE);
+    }
+}
+
+lv_event_cb_t BackKeyProcess(lv_event_t *Event)
+{
+    lv_event_code_t Code = lv_event_get_code(Event);
+
+    if( Code == LV_EVENT_CLICKED)
+    {
+        printf("Back !! \r\n");
+        lv_obj_set_style_bg_color(Bg,lv_color_black(),LV_PART_MAIN);
+    }
+}
+
+//***************************************************//
+//  功能描述: LVGL 绘图和布局
+//  
+//  参数:  
+//  
+//  返回值: 
+//  
+//  说明: 无
+//  
+//***************************************************//
+void Btn()
+{
+    lv_obj_t *Btn = lv_btn_create(lv_scr_act());
+    lv_obj_align(Btn,LV_ALIGN_CENTER,0,0);
+    lv_obj_set_size(Btn,100,100);
+    lv_obj_set_style_bg_color(Btn,lv_palette_main( LV_PALETTE_BLUE),0);
+    lv_obj_set_style_bg_grad_color(Btn, lv_palette_main(LV_PALETTE_RED), 0);
+    lv_obj_set_style_bg_grad_dir(Btn, LV_GRAD_DIR_HOR, 0);
+    lv_obj_set_style_border_width(Btn,3,LV_PART_MAIN);
+    lv_obj_set_style_shadow_color(Btn,lv_color_black(),0);
+    lv_obj_set_style_shadow_ofs_x(Btn,3,0);
+    lv_obj_add_flag(Btn,LV_OBJ_FLAG_CHECKABLE);
+    // lv_obj_add_event_cb(Btn,Btn_Handle,LV_EVENT_ALL,NULL);
+
+    lv_obj_t *label_A = lv_label_create(Btn);
+    lv_label_set_text(label_A,"1 2 3 4 5 6 7 8 9 ");
+    lv_obj_set_align(label_A,LV_ALIGN_TOP_MID);
+    lv_label_set_long_mode(label_A, LV_LABEL_LONG_SCROLL );
+    lv_obj_set_size(label_A,40,50);
+
+
+    lv_obj_t *label_B = lv_label_create(Btn);
+    lv_label_set_text(label_B,"1 2 3 4 5 6 7 8 9 ");
+    lv_obj_set_align(label_B,LV_ALIGN_BOTTOM_MID);
+    lv_label_set_long_mode(label_B, LV_LABEL_LONG_SCROLL );
+    lv_obj_set_size(label_B,40,50);
+
+}
+
+
+void KeyBtn_OK()
+{
+    Btn_OK = lv_btn_create(lv_scr_act());
+    lv_obj_t *Lable = lv_label_create(Btn_OK);
+
+    lv_obj_set_size(Btn_OK,30,20);
+    lv_obj_set_pos(Btn_OK,5,190);
+    lv_obj_set_style_border_side(Btn_OK,LV_BORDER_SIDE_FULL,LV_PART_MAIN);
+    lv_obj_set_style_border_width(Btn_OK,2,LV_PART_MAIN);
+
+    // lv_obj_set_style_shadow_color(Btn_OK,lv_color_white(),LV_PART_MAIN );
+    // lv_obj_set_style_shadow_ofs_x(Btn_OK,2,LV_PART_MAIN );
+    // lv_obj_set_style_shadow_ofs_y(Btn_OK,2,LV_PART_MAIN );
+    // lv_obj_set_style_shadow_color(Btn_OK,lv_color_black(),LV_PART_MAIN | LV_STATE_PRESSED);
+
+    //lv_label_set_text(Lable,"OK");
+    lv_label_set_text(Lable,LV_SYMBOL_OK);
+    lv_obj_set_align(Lable,LV_ALIGN_CENTER);
+
+    lv_obj_add_event_cb(Btn_OK, OKKeyProcess ,LV_EVENT_CLICKED,NULL);
+
+}
+
+void  KeyBtn_BACK()
+{
+    Btn_BACK = lv_btn_create(lv_scr_act());
+
+    lv_obj_t *Lable = lv_label_create(Btn_BACK);
+
+    lv_obj_set_size(Btn_BACK,30,20);
+    lv_obj_set_pos(Btn_BACK,205,190);
+    lv_obj_set_style_border_side(Btn_BACK,LV_BORDER_SIDE_FULL,LV_PART_MAIN);
+    lv_obj_set_style_border_width(Btn_BACK,2,LV_PART_MAIN);
+
+    // lv_obj_set_style_shadow_color(Btn_BACK,lv_color_white(),LV_PART_MAIN );
+    // lv_obj_set_style_shadow_ofs_x(Btn_BACK,2,LV_PART_MAIN );
+    // lv_obj_set_style_shadow_ofs_y(Btn_BACK,2,LV_PART_MAIN );
+    // lv_obj_set_style_shadow_color(Btn_BACK,lv_color_black(),LV_PART_MAIN | LV_STATE_PRESSED);
+
+    // lv_label_set_text(Lable,"Back");
+    lv_label_set_text(Lable,LV_SYMBOL_CLOSE);
+    lv_obj_set_align(Lable,LV_ALIGN_CENTER);
+
+    lv_obj_add_event_cb(Btn_BACK, BackKeyProcess ,LV_EVENT_CLICKED,NULL);
+
+
+}
+
+void  KeyBtn_UP()
+{
+    Btn_UP = lv_btn_create(lv_scr_act());
+    lv_obj_t *Lable = lv_label_create(Btn_UP);
+
+    lv_obj_set_size(Btn_UP,30,20);
+    lv_obj_set_pos(Btn_UP,5,215);
+    lv_obj_set_style_border_side(Btn_UP,LV_BORDER_SIDE_FULL,LV_PART_MAIN);
+    lv_obj_set_style_border_width(Btn_UP,2,LV_PART_MAIN);
+
+    // lv_obj_set_style_shadow_color(Btn_UP,lv_color_white(),LV_PART_MAIN );
+    // lv_obj_set_style_shadow_ofs_x(Btn_UP,2,LV_PART_MAIN );
+    // lv_obj_set_style_shadow_ofs_y(Btn_UP,2,LV_PART_MAIN );
+    // lv_obj_set_style_shadow_color(Btn_UP,lv_color_black(),LV_PART_MAIN | LV_STATE_PRESSED);
+
+    // lv_label_set_text(Lable,"Up");
+    lv_label_set_text(Lable,LV_SYMBOL_UP);
+    lv_obj_set_align(Lable,LV_ALIGN_CENTER);
+
+    lv_obj_add_event_cb(Btn_UP, UpKeyProcess ,LV_EVENT_CLICKED,NULL);
+}
+
+
+
+
+void  KeyBtn_DOWN()
+{
+    Btn_DOWN = lv_btn_create(lv_scr_act());
+    lv_obj_t *Lable = lv_label_create(Btn_DOWN);
+
+    lv_obj_set_size(Btn_DOWN,30,20);
+    lv_obj_set_pos(Btn_DOWN,205,215);
+
+    lv_obj_set_style_border_side(Btn_DOWN,LV_BORDER_SIDE_FULL,LV_PART_MAIN);
+    lv_obj_set_style_border_width(Btn_DOWN,2,LV_PART_MAIN);
+
+    // lv_obj_set_style_shadow_color(Btn_DOWN,lv_color_white(),LV_PART_MAIN );
+    // lv_obj_set_style_shadow_ofs_x(Btn_DOWN,2,LV_PART_MAIN );
+    // lv_obj_set_style_shadow_ofs_y(Btn_DOWN,2,LV_PART_MAIN );
+    // lv_obj_set_style_shadow_color(Btn_DOWN,lv_color_black(),LV_PART_MAIN | LV_STATE_PRESSED);
+
+    // lv_label_set_text(Lable,"Down");
+    lv_label_set_text(Lable,LV_SYMBOL_DOWN);
+    lv_obj_set_align(Lable,LV_ALIGN_CENTER);
+
+    lv_obj_add_event_cb(Btn_DOWN, DownKeyProcess ,LV_EVENT_CLICKED,NULL);
+
+}
+
+
+
+void BackGroung()
+{
+    Bg = lv_obj_create(lv_scr_act());
+    lv_obj_set_size(Bg,240,240);
+    lv_obj_set_style_bg_color(Bg,lv_color_black(),LV_PART_MAIN);
+    lv_obj_set_style_radius(Bg,0,LV_PART_MAIN);
+
+    // lv_obj_set_style_bg_grad_color(Bg, lv_palette_main(LV_PALETTE_GREY), 0);
+    // lv_obj_set_style_bg_grad_dir(Bg, LV_GRAD_DIR_VER, 0);
+
+//    lv_obj_t *boxPad = lv_canvas_create(lv_scr_act());
+//    lv_obj_set_size(boxPad,20,20);
+   
+}
+
+void LVGL_Line()
+{
+    lv_obj_t *Line = lv_line_create(lv_scr_act());
+}
+
+
+
+
+
+
+
+//***************************************************//
+//  功能描述: LVGL UI绘制
+//  
+//  参数: 无
+//  
+//  返回值: 无
+//  
+//  说明: 无
+//  
+//***************************************************//
+static void LVGL_Build_GUI()
+{
+    BackGroung();
+    KeyBtn_OK();
+    KeyBtn_BACK();
+    KeyBtn_UP();
+    KeyBtn_DOWN();
+    Btn();
+}
+
+
+//***************************************************//
+//  功能描述: LVGL Demo测试
+//  
+//  参数: 无
+//  
+//  返回值: 无
+//  
+//  说明: 无
+//  
+//***************************************************//
+void LVGL_Demo()
+{
+	// lv_ex_keyboard_1();
+	// lv_demo_stress();
+	// lv_demo_benchmark();
+	// lv_demo_keypad_encoder();
+}
+
+//***************************************************//
+//  功能描述: LVGL初始化
+//  
+//  参数: 无
+//  
+//  返回值: 无
+//  
+//  说明: 无
+//  
+//***************************************************//
+void LVGL_Init()
+{
+#ifndef ON_PC
+    lv_init();
+	lv_port_disp_init();
+	lv_port_indev_init();
+#endif
+
+    LVGL_Build_GUI();
+    //LVGL_Demo();
+}
+
+
+//***************************************************//
+//  功能描述: LVGL定时任务
+//  
+//  参数: 无
+//  
+//  返回值: 无
+//  
+//  说明: 无
+//  
+//***************************************************//
+void LVGL_Task()
+{
+    lv_task_handler();
+}
+
+
+
---
+++ Module/LVGL/LVGL_UI.h
@@ -0,0 +1,12 @@
+#ifndef  KEYPAD_H
+#define  KEYPAD_H
+
+#define ON_PC
+
+
+
+void LVGL_Init(void);
+void LVGL_Task(void);
+#endif
+
+
