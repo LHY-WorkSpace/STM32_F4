e6967b03525f5b7565ecc83f94e6f90653c7ec4d
Decktop <2371270263@qq.com>
2022-11-21 18:27:23 +0800

    1.完善编码器代码

--- Driver/Encoder.c
+++ Driver/Encoder.c
@@ -3,7 +3,7 @@
 //转一周多少个信号
 #define ENCODER_LINES   (10)
 
-
+EnCoderInfo_t EnCoderInfo;
 
 // Timer_2
 //  D12 D13 
@@ -11,7 +11,7 @@ void EncoderInit()
 {
 
 	GPIO_InitTypeDef Encoder_GPIO_Init;
-
+	NVIC_InitTypeDef  NVIC_Initstr;
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStr;
 
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4,ENABLE);
@@ -31,10 +31,33 @@ void EncoderInit()
     GPIO_PinAFConfig(GPIOD,GPIO_PinSource12,GPIO_AF_TIM4);
     GPIO_PinAFConfig(GPIOD,GPIO_PinSource13,GPIO_AF_TIM4);
 
+	NVIC_Initstr.NVIC_IRQChannel=TIM4_IRQn;
+	NVIC_Initstr.NVIC_IRQChannelPreemptionPriority=8;
+	NVIC_Initstr.NVIC_IRQChannelSubPriority=0;
+	NVIC_Initstr.NVIC_IRQChannelCmd=ENABLE;
+	NVIC_Init(&NVIC_Initstr);
+
     //T1 T2 均计数(4倍频计数)，IC1 不反相,IC2 不反相
     TIM_EncoderInterfaceConfig(TIM4,TIM_EncoderMode_TI12,TIM_ICPolarity_Rising,TIM_ICPolarity_Rising);
 
+    TIM_ClearITPendingBit(TIM4,TIM_IT_Update );
+    TIM_ITConfig(TIM4,TIM_IT_Update,ENABLE);
+    TIM_Cmd(TIM4,ENABLE);
+
+}
+
+static void EnCoderUpData()
+{
+    if( ( TIM4->CR1  ) & 0x10)
+    {
+        EnCoderInfo.Dir = Encoder_UP;
+    }
+    else
+    {
+        EnCoderInfo.Dir = Encoder_DOWN;
+    }
     
+    EnCoderInfo.Value = TIM4->CNT;
 }
 
 
@@ -43,9 +66,31 @@ void EncoderInit()
 
 
 
+void EnCoderSetValue(u16 Data)
+{
+    TIM4->CNT = Data;
+    EnCoderInfo.Value = Data;
+}
+
+void EnCoderGetValue()
+{
+    return EnCoderInfo.Value;
+}
+
+u8 EnCoderGetDir()
+{
+    return EnCoderInfo.Dir;
+}
 
 
-
+void TIM4_IRQHandler()
+{
+    if( TIM_GetITStatus(TIM4,TIM_IT_Update) == SET)
+    {
+        TIM_ClearITPendingBit(TIM4,TIM_IT_Update );
+        EnCoderUpData();
+    }
+}
 
 
 
--- Driver/Encoder.h
+++ Driver/Encoder.h
@@ -3,6 +3,23 @@
 // #include "stm32f4xx.h"
 
 
+typedef enum
+{
+    Encoder_UP,
+    Encoder_DOWN
+
+}Encoder_Dir_e;
+
+
+
+typedef struct
+{
+    Encoder_Dir_e Dir;
+    u16 Value;
+}EnCoderInfo_t;
+
+
+
 void EncoderInit(void);
 
 
